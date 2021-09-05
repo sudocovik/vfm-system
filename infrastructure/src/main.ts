ab152f6b728c33929b54b6ff1f59be849f601a85
covik <mate.nakic3@gmail.com>
2021-09-05 22:47:25 +0200

    Add LocalProvisioner with create k3d cluster functionality

--- infrastructure/src/main.ts
+++ infrastructure/src/main.ts
@@ -1,8 +1,10 @@
 import provisionResources from './pulumi/provision'
+import { LocalProvisioner } from './provisioners/LocalProvisioner'
 
 if (process.env.NODE_ENV === 'production') {
     provisionResources().then(() => console.log('Provisioned all resources')).catch(error => console.log(error))
 }
 else if (process.env.NODE_ENV === 'local') {
     console.log('Running in local mode...')
+    new LocalProvisioner().provision()
 }
