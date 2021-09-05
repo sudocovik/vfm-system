ba1ac25f72b11ceaecc56a0f02575e06124140ea
covik <mate.nakic3@gmail.com>
2021-09-05 20:43:59 +0200

    Start different programs depending on environment

--- infrastructure/src/main.ts
+++ infrastructure/src/main.ts
@@ -1,3 +1,8 @@
 import provisionResources from './pulumi/provision'
 
-provisionResources().then(() => console.log('Provisioned all resources')).catch(error => console.log(error))
+if (process.env.NODE_ENV === 'production') {
+    provisionResources().then(() => console.log('Provisioned all resources')).catch(error => console.log(error))
+}
+else if (process.env.NODE_ENV === 'local') {
+    console.log('Running in local mode...')
+}
