f0a73c186f90f8ba35deb0f6e2c0b4d425d8964d
covik <mate.nakic3@gmail.com>
2021-09-20 12:48:34 +0200

    Fix failing CI appears as successful

--- infrastructure/src/main.ts
+++ infrastructure/src/main.ts
@@ -6,7 +6,6 @@ import { createKubernetesManifests } from './pulumi/create-kubernetes-manifests'
 if (process.env.NODE_ENV === 'production') {
     provisionResources(async () => createCloudResources().then(createKubernetesManifests))
         .then(() => console.log('Provisioned all resources'))
-        .catch(error => console.log(error))
 }
 else if (process.env.NODE_ENV === 'local') {
     console.log('Running in local mode...')
