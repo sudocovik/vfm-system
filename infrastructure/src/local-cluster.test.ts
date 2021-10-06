e7b936b088d6e80c2ac871723ae793ed40cddc4c
covik <mate.nakic3@gmail.com>
2021-10-06 11:41:10 +0200

    Make cluster instance local for test case

--- infrastructure/src/local-cluster.test.ts
+++ infrastructure/src/local-cluster.test.ts
@@ -1,13 +1,8 @@
 import { k3dCluster } from './local-cluster'
 
 describe('#local cluster', () => {
-    let cluster: k3dCluster
-
-    beforeAll(() => {
-        cluster = new k3dCluster()
-    })
-
     test('creating existing cluster should be idempotent', async () => {
+        const cluster: k3dCluster = new k3dCluster()
         await cluster.create()
         expect(() => cluster.create()).not.toThrow()
     }, 1000 * 60 * 2)
