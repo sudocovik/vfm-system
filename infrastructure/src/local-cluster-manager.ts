00ed0bae790ccd547b5871950291b52b687ba671
covik <mate.nakic3@gmail.com>
2021-10-06 18:24:49 +0200

    Move destroy() closer to create()

--- infrastructure/src/local-cluster-manager.ts
+++ infrastructure/src/local-cluster-manager.ts
@@ -1,13 +1,13 @@
 export interface LocalCluster {
     create(): Promise<void>
 
+    destroy(): Promise<void>
+
     exists(): Promise<boolean>
 
     start(): Promise<void>
 
     stop(): Promise<void>
-
-    destroy(): Promise<void>
 
     kubeconfig(): Promise<string>
 }
