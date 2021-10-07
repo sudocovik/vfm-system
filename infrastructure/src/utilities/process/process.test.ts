742d5f18448d69908df071f8182d75a7e8bd6aa7
covik <mate.nakic3@gmail.com>
2021-10-07 16:50:44 +0200

    Make sure process run() bubbles up exceptions

--- infrastructure/src/utilities/process/process.test.ts
+++ infrastructure/src/utilities/process/process.test.ts
@@ -1,6 +1,12 @@
 import { SimpleProcess } from './process'
 
 describe('#process', () => {
+  test('run() should bubble up exceptions', async () => {
+    await expect(
+        new SimpleProcess().run(async () => { throw new Error })
+    ).rejects.toThrow()
+  })
+
   test('should be runnable', () => {
     let hasRun: boolean = false
 
