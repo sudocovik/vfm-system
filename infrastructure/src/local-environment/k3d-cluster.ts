afdf098c5fb996059da8494c245e672edc9cccd4
covik <mate.nakic3@gmail.com>
2021-10-07 11:31:11 +0200

    Add script for launching local cluster

---
+++ infrastructure/src/local-environment/k3d-cluster.ts
@@ -0,0 +1,31 @@
+import { promisify } from 'util'
+import { LocalCluster } from './local-cluster-manager'
+const exec = promisify(require('child_process').exec)
+
+export class k3dCluster implements LocalCluster {
+    public async create(): Promise<void> {
+        await exec('k3d cluster create vfm')
+    }
+
+    public async destroy(): Promise<void> {
+        await exec('k3d cluster delete vfm')
+    }
+
+    public async exists(): Promise<boolean> {
+        const { stdout: result } = await exec('k3d cluster list')
+        return result.indexOf('vfm') !== -1
+    }
+
+    public async start(): Promise<void> {
+        await exec('k3d cluster start vfm')
+    }
+
+    public async stop(): Promise<void> {
+        await exec('k3d cluster stop vfm')
+    }
+
+    public async kubeconfig(): Promise<string> {
+        const { stdout } = await exec('k3d kubeconfig get vfm')
+        return stdout
+    }
+}
---
+++ infrastructure/src/local.ts
@@ -0,0 +1,21 @@
+import { LocalClusterManager } from './local-environment/local-cluster-manager'
+import { k3dCluster } from './local-environment/k3d-cluster'
+
+const clusterManager = new LocalClusterManager(new k3dCluster())
+
+process.stdout.write('\x1b[33m' + 'Starting cluster...' + '\x1b[0m')
+clusterManager.launch().then(() => {
+    process.stdout.moveCursor(0, -1)
+    process.stdout.clearScreenDown()
+    process.stdout.write('\n' + '\x1b[32m' + 'Cluster running' + '\x1b[0m')
+})
+
+process.on('SIGINT', () => {
+    console.log("\nstopping cluster...")
+    clusterManager.stop().then(() => {
+        console.log('cluster stopped!')
+        process.exit(0)
+    })
+})
+
+setInterval(() => {}, 30000)
