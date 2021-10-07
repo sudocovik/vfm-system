0c89012fabaaa8528d32d1a3e6db220a5be821c9
covik <mate.nakic3@gmail.com>
2021-10-07 11:41:53 +0200

    Remove old "implementation" of local cluster provisioning

--- infrastructure/src/main.ts
+++ infrastructure/src/main.ts
@@ -1,15 +1,8 @@
 import provisionResources from './pulumi/provision'
 import { createCloudResources } from './pulumi/create-resources'
-import { LocalClusterProvisioner } from './provisioners/LocalClusterProvisioner'
 import { createKubernetesManifests } from './pulumi/create-kubernetes-manifests'
 
 const clusterToken: string = process.env.CLUSTER_TOKEN || ''
 
-if (process.env.NODE_ENV === 'production') {
-    provisionResources(async () => createCloudResources(clusterToken).then(createKubernetesManifests))
-        .then(() => console.log('Provisioned all resources'))
-}
-else if (process.env.NODE_ENV === 'local') {
-    console.log('Running in local mode...')
-    new LocalClusterProvisioner().provision()
-}
+provisionResources(async () => createCloudResources(clusterToken).then(createKubernetesManifests))
+    .then(() => console.log('Provisioned all resources'))
