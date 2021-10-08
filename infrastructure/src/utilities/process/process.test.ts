26990e880633d125cc9e55028c53a32e90bd8cd3
covik <mate.nakic3@gmail.com>
2021-10-08 20:42:46 +0200

    Group related tests

--- infrastructure/src/utilities/process/process.test.ts
+++ infrastructure/src/utilities/process/process.test.ts
@@ -9,27 +9,31 @@ class TestProcess extends SimpleProcess {
 }
 
 describe('#process', () => {
-  test('run() should bubble up exceptions', async () => {
-    await expect(
-        new TestProcess().run(async () => { throw new Error })
-    ).rejects.toThrow()
+  describe('main functionality', () => {
+    it('should execute callback passed as argument', () => {
+      let hasRun: boolean = false
+
+      new TestProcess().run(async () => { hasRun = true })
+
+      expect(hasRun).toBe(true)
+    })
+
+    it('should bubble up exceptions', async () => {
+      await expect(
+          new TestProcess().run(async () => { throw new Error })
+      ).rejects.toThrow()
+    })
   })
 
-  test('should be runnable', () => {
-    let hasRun: boolean = false
+  describe('graceful shutdown handling', () => {
+    it('should execute callback passed as argument', async () => {
+      const process: CustomProcess = new TestProcess()
+      let hasRun: boolean = false
 
-    new TestProcess().run(async () => { hasRun = true })
+      process.onGracefulShutdownRequest(async () => { hasRun = true })
+      await process.run(async () => {})
 
-    expect(hasRun).toBe(true)
-  })
-
-  test('can handle SIGINT', async () => {
-    const process: CustomProcess = new TestProcess()
-    let hasRun: boolean = false
-
-    process.onGracefulShutdownRequest(async () => { hasRun = true })
-    await process.run(async () => {})
-
-    expect(hasRun).toBe(true)
+      expect(hasRun).toBe(true)
+    })
   })
 })
