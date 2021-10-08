975995267b5b499d5fb01fc2411e39b1699ee885
covik <mate.nakic3@gmail.com>
2021-10-08 22:39:13 +0200

    Wait 60s for cluster to create and do not deploy traefik

--- infrastructure/src/local-environment/k3d-cluster.ts
+++ infrastructure/src/local-environment/k3d-cluster.ts
@@ -4,7 +4,7 @@ const exec = promisify(require('child_process').exec)
 
 export class k3dCluster implements LocalCluster {
     public async create(): Promise<void> {
-        await exec('k3d cluster create vfm')
+        await exec('k3d cluster create vfm --wait --timeout=60s --k3s-arg=\'--no-deploy=traefik\'@server:*')
     }
 
     public async destroy(): Promise<void> {
