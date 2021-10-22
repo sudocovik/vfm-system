034536103f9215dd7cb9716ecb0647cf0424a98d
covik <mate.nakic3@gmail.com>
2021-10-22 12:33:50 +0200

    Simplify forbidden argument type test

--- infrastructure/src/pulumi/Stack.test.ts
+++ infrastructure/src/pulumi/Stack.test.ts
@@ -1,103 +1,29 @@
 import { ProductionStack } from './Stack'
 
 describe('#ProductionStack', () => {
-    it('should not accept null', () => {
-        expect.assertions(1)
-        try {
-            new ProductionStack(null as any)
-        }
-        catch (e) {
-            expect(e).toBeInstanceOf(TypeError)
-        }
-    })
+    const forbiddenTypes = [
+        { type: 'null', value: null },
+        { type: 'undefined', value: undefined },
+        { type: 'object', value: {} },
+        { type: 'array', value: [] },
+        { type: 'integer', value: 1234 },
+        { type: 'float', value: 3.14 },
+        { type: 'true', value: true },
+        { type: 'false', value: false },
+        { type: 'bigint', value: 22n ** 53n },
+        { type: 'symbol', value: Symbol('test') }
+    ]
 
-    it('should not accept undefined', () => {
-        expect.assertions(1)
-        try {
-            new ProductionStack(undefined as any)
-        }
-        catch (e) {
-            expect(e).toBeInstanceOf(TypeError)
-        }
-    })
-
-    it('should not accept object', () => {
-        expect.assertions(1)
-        try {
-            new ProductionStack({} as any)
-        }
-        catch (e) {
-            expect(e).toBeInstanceOf(TypeError)
-        }
-    })
-
-    it('should not accept array', () => {
-        expect.assertions(1)
-        try {
-            new ProductionStack([] as any)
-        }
-        catch (e) {
-            expect(e).toBeInstanceOf(TypeError)
-        }
-    })
-
-    it('should not accept integer', () => {
-        expect.assertions(1)
-        try {
-            new ProductionStack( 1234 as any)
-        } catch (e) {
-            expect(e).toBeInstanceOf(TypeError)
-        }
-    })
-
-    it('should not accept float', () => {
-        expect.assertions(1)
-        try {
-            new ProductionStack(3.14 as any)
-        }
-        catch (e) {
-            expect(e).toBeInstanceOf(TypeError)
-        }
-    })
-
-    it('should not accept true', () => {
-        expect.assertions(1)
-        try {
-            new ProductionStack(true as any)
-        }
-        catch (e) {
-            expect(e).toBeInstanceOf(TypeError)
-        }
-    })
-
-    it('should not accept false', () => {
-        expect.assertions(1)
-        try {
-            new ProductionStack(false as any)
-        }
-        catch (e) {
-            expect(e).toBeInstanceOf(TypeError)
-        }
-    })
-
-    it('should not accept big integers', () => {
-        expect.assertions(1)
-        try {
-            new ProductionStack(22n ** 53n as any)
-        }
-        catch (e) {
-            expect(e).toBeInstanceOf(TypeError)
-        }
-    })
-
-    it('should not accept symbols', () => {
-        expect.assertions(1)
-        try {
-            new ProductionStack(Symbol('test') as any)
-        }
-        catch (e) {
-            expect(e).toBeInstanceOf(TypeError)
-        }
+    forbiddenTypes.forEach(({ type, value }) => {
+        it(`should not accept '${type}'`, () => {
+            expect.assertions(1)
+            try {
+                new ProductionStack(value as any)
+            }
+            catch (e) {
+                expect(e).toBeInstanceOf(TypeError)
+            }
+        })
     })
 
     it('should accept string', () => {
