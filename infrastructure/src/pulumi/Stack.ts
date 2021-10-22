2b92f42d64718af7fae648b0062111b799bc07cf
covik <mate.nakic3@gmail.com>
2021-10-22 16:11:54 +0200

    Simplify type-checking

--- infrastructure/src/pulumi/Stack.ts
+++ infrastructure/src/pulumi/Stack.ts
@@ -1,24 +1,12 @@
+const stringRepresentationOfVariableType = (value: any) => {
+    // typeof null returns 'object' so null has to be given special treatment...
+    if (value === null) return 'null'
+    return typeof value
+}
+
 export class ProductionStack {
     constructor(public readonly name: string) {
-        if (typeof name === 'number')
-            throw new TypeError('Stack name should be string, got number')
-
-        if (name === null)
-            throw new TypeError('Stack name should be string, got null')
-
-        if (name === undefined)
-            throw new TypeError('Stack name should be string, got undefined')
-
-        if (typeof name === 'object')
-            throw new TypeError('Stack name should be string, got object')
-
-        if (typeof name === 'boolean')
-            throw new TypeError('Stack name should be string, got boolean')
-
-        if (typeof name === 'bigint')
-            throw new TypeError('Stack name should be string, got bigint')
-
-        if (typeof name === 'symbol')
-            throw new TypeError('Stack name should be string, got symbol')
+        if (typeof name !== 'string')
+            throw new TypeError('Stack name should be string, got ' + stringRepresentationOfVariableType(name))
     }
 }
