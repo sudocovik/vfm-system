7dbe2afb1752bededcaa3804e9d8732736df5a13
covik <mate.nakic3@gmail.com>
2021-10-28 17:32:36 +0200

    Start new implementation of describing infrastructure resources

---
+++ infrastructure/src/production/backbone-types.ts
@@ -0,0 +1,41 @@
+export type DomainConfiguration = {
+    name: string
+}
+
+type SinglePort = {
+    external: number,
+    internal: number
+}
+
+type Ports = {
+    http: SinglePort
+    https: SinglePort
+    teltonika: SinglePort
+}
+
+export type LoadBalancerConfiguration = {
+    size: string
+    region: string
+    ports: Ports
+}
+
+type NodePool = {
+    name: string
+    size: string
+    count: number
+    tag: string
+}
+
+export type ClusterConfiguration = {
+    name: string
+    version: string
+    region: string
+    nodePool: NodePool
+}
+
+export type ProjectConfiguration = {
+    name: string
+    description: string
+    purpose: string
+    environment: string
+}
---
+++ infrastructure/src/production/backbone.ts
@@ -0,0 +1,125 @@
+import { Certificate, DnsRecord, Domain, KubernetesCluster, LoadBalancer, Project } from '@pulumi/digitalocean'
+import type {
+    ClusterConfiguration,
+    DomainConfiguration,
+    LoadBalancerConfiguration,
+    ProjectConfiguration,
+} from './backbone-types'
+import * as pulumi from '@pulumi/pulumi'
+
+export function generateKubeconfig(
+    cluster: KubernetesCluster,
+    user: pulumi.Input<string>,
+    apiToken: pulumi.Input<string>,
+): pulumi.Output<string> {
+    const clusterName = pulumi.interpolate`do-${cluster.region}-${cluster.name}`
+
+    return pulumi.interpolate`apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data: ${cluster.kubeConfigs[0].clusterCaCertificate}
+    server: ${cluster.endpoint}
+  name: ${clusterName}
+contexts:
+- context:
+    cluster: ${clusterName}
+    user: ${clusterName}-${user}
+  name: ${clusterName}
+current-context: ${clusterName}
+kind: Config
+users:
+- name: ${clusterName}-${user}
+  user:
+    token: ${apiToken}
+`;
+}
+
+export function describeBackboneResources(
+    domainConfiguration: DomainConfiguration,
+    loadBalancerConfiguration: LoadBalancerConfiguration,
+    clusterConfiguration: ClusterConfiguration,
+    projectConfiguration: ProjectConfiguration,
+    apiToken: string
+): any {
+    const domain = new Domain('main-domain', {
+        name: domainConfiguration.name
+    })
+
+    const certificate = new Certificate('main-certificate', {
+        type: 'lets_encrypt',
+        domains: [
+            domain.name,
+            domain.name.apply(domainName => `*.${domainName}`)
+        ]
+    })
+
+    const loadBalancer = new LoadBalancer('main-load-balancer', {
+        size: loadBalancerConfiguration.size,
+        region: loadBalancerConfiguration.region,
+        dropletTag: clusterConfiguration.nodePool.tag,
+        redirectHttpToHttps: true,
+        forwardingRules: [{
+            entryPort: loadBalancerConfiguration.ports.http.external,
+            entryProtocol: 'http',
+            targetPort: loadBalancerConfiguration.ports.http.internal,
+            targetProtocol: 'http'
+        }, {
+            entryPort: loadBalancerConfiguration.ports.https.external,
+            entryProtocol: 'https',
+            targetPort: loadBalancerConfiguration.ports.https.internal,
+            targetProtocol: 'http',
+            certificateName: certificate.name,
+        }, {
+            entryPort: loadBalancerConfiguration.ports.teltonika.external,
+            entryProtocol: 'tcp',
+            targetPort: loadBalancerConfiguration.ports.teltonika.internal,
+            targetProtocol: 'tcp'
+        }],
+        healthcheck: {
+            path: '/',
+            port: loadBalancerConfiguration.ports.http.internal,
+            protocol: 'http',
+            checkIntervalSeconds: 10,
+            responseTimeoutSeconds: 5,
+            unhealthyThreshold: 3,
+            healthyThreshold: 5
+        }
+    })
+
+    new DnsRecord('main-subdomains', {
+        domain: domain.name,
+        name: '*',
+        type: 'A',
+        value: loadBalancer.ip
+    })
+
+    const cluster = new KubernetesCluster('main-cluster', {
+        name: clusterConfiguration.name,
+        version: clusterConfiguration.version,
+        region: clusterConfiguration.region,
+        autoUpgrade: false,
+        nodePool: {
+            name: clusterConfiguration.nodePool.name,
+            size: clusterConfiguration.nodePool.size,
+            autoScale: false,
+            nodeCount: clusterConfiguration.nodePool.count,
+            tags: [clusterConfiguration.nodePool.tag]
+        }
+    })
+
+    new Project('main-project', {
+        name: projectConfiguration.name,
+        environment: projectConfiguration.environment,
+        description: projectConfiguration.description,
+        purpose: projectConfiguration.purpose,
+        resources: [
+            domain.domainUrn,
+            cluster.clusterUrn,
+            loadBalancer.loadBalancerUrn
+        ]
+    })
+
+    return {
+        kubeconfig: generateKubeconfig(cluster, 'admin', apiToken)
+    }
+}
