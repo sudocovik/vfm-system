0b191361c3107d8a48b2cb6ca349f8d0d35328f6
covik <mate.nakic3@gmail.com>
2021-10-29 23:03:25 +0200

    Deploy traccar resources

--- infrastructure/src/production/backend.ts
+++ infrastructure/src/production/backend.ts
@@ -1,6 +1,7 @@
 import provision from '../pulumi/provision'
 import { DatabaseCluster, DatabaseDb, DatabaseFirewall, DatabaseUser } from '@pulumi/digitalocean'
 import * as pulumi from '@pulumi/pulumi'
+import * as k8s from '@pulumi/kubernetes'
 
 function describeDatabase(kubernetesCluster: pulumi.Output<any>): void {
     const cluster = new DatabaseCluster('main-backend-database', {
@@ -35,11 +36,120 @@ function describeDatabase(kubernetesCluster: pulumi.Output<any>): void {
     })
 }
 
+function describeApplication(provider: k8s.Provider, namespace: pulumi.Output<any>): void {
+    const labels = { app: 'traccar' }
+
+    const deployment: k8s.apps.v1.Deployment = new k8s.apps.v1.Deployment('traccar-deployment', {
+        metadata: {
+            namespace
+        },
+        spec: {
+            selector: {
+                matchLabels: labels
+            },
+            template: {
+                metadata: {
+                    labels:  labels
+                },
+                spec: {
+                    restartPolicy: 'Always',
+                    containers: [{
+                        name: 'backend',
+                        image: 'traccar/traccar:4.13-alpine',
+                        imagePullPolicy: 'IfNotPresent',
+                        ports: [{
+                            name: 'api',
+                            containerPort: 8082,
+                            protocol: 'TCP'
+                        }, {
+                            name: 'teltonika',
+                            containerPort: 5027,
+                            protocol: 'TCP'
+                        }],
+                        startupProbe: {
+                            httpGet: {
+                                path: '/',
+                                port: 'api',
+                                scheme: 'HTTP'
+                            },
+                            failureThreshold: 30,
+                            periodSeconds: 30,
+                        }
+                    }]
+                }
+            }
+        }
+    }, { provider })
+
+    new k8s.core.v1.Service('traccar-teltonika-service', {
+        metadata: {
+            namespace
+        },
+        spec: {
+            type: 'NodePort',
+            selector: labels,
+            ports: [{
+                name: 'teltonika',
+                port: 5027,
+                nodePort: 32027,
+                targetPort: deployment.spec.template.spec.containers[0].ports[1].name,
+                protocol: 'TCP'
+            }]
+        }
+    }, { provider })
+
+    const service: k8s.core.v1.Service = new k8s.core.v1.Service('traccar-http-service', {
+        metadata: {
+            namespace
+        },
+        spec: {
+            selector: labels,
+            ports: [{
+                name: 'api',
+                port: 80,
+                targetPort: deployment.spec.template.spec.containers[0].ports[0].name,
+                protocol: 'TCP'
+            }]
+        }
+    }, { provider })
+
+    new k8s.networking.v1.Ingress('default-ingress', {
+        metadata: {
+            namespace
+        },
+        spec: {
+            rules: [{
+                http: {
+                    paths: [{
+                        path: '/api',
+                        pathType: 'Prefix',
+                        backend: {
+                            service: {
+                                name: service.metadata.name,
+                                port: {
+                                    name: service.spec.ports[0].name
+                                }
+                            }
+                        }
+                    }]
+                }
+            }]
+        }
+    }, { provider })
+}
+
 function describeBackendResources(): any {
     const backbone = new pulumi.StackReference('covik/vfm/backbone-production')
+    const kubeconfig = backbone.getOutput('kubeconfig')
+    const namespaceName = backbone.getOutput('namespaceName')
     const kubernetesClusterId = backbone.getOutput('clusterId')
 
+    const provider: k8s.Provider = new k8s.Provider('main-kubernetes-provider', {
+        kubeconfig
+    })
+
     describeDatabase(kubernetesClusterId)
+    describeApplication(provider, namespaceName)
 }
 
 export function deployBackendResources(): void {
