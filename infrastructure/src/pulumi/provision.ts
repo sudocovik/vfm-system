017be60921a5904a6d08a20361d39e0b1dd9cc92
covik <mate.nakic3@gmail.com>
2021-10-30 20:48:42 +0200

    Do not hardcode plugin version, read 'em from the package-lock.json

--- infrastructure/src/pulumi/provision.ts
+++ infrastructure/src/pulumi/provision.ts
@@ -1,5 +1,8 @@
 import { InlineProgramArgs, LocalWorkspace, Stack } from '@pulumi/pulumi/automation'
 
+const installedDependencies = require('../../package-lock.json').dependencies
+const findDependencyVersion = (wantedDependency: string) => installedDependencies[wantedDependency].version
+
 export default async function (stackName: string, program: () => Promise<any>) {
   const stackArguments: InlineProgramArgs = {
     projectName: 'vfm',
@@ -9,8 +12,8 @@ export default async function (stackName: string, program: () => Promise<any>) {
 
   const stack: Stack = await LocalWorkspace.createOrSelectStack(stackArguments)
 
-  await stack.workspace.installPlugin('digitalocean', '4.6.0')
-  await stack.workspace.installPlugin('kubernetes', '3.7.0')
+  await stack.workspace.installPlugin('digitalocean', findDependencyVersion('@pulumi/digitalocean'))
+  await stack.workspace.installPlugin('kubernetes', findDependencyVersion('@pulumi/kubernetes'))
   await stack.refresh({ onOutput: console.info })
   await stack.up({ onOutput: console.info })
 }
