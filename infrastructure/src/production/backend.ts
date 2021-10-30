4b8b938c00b701c3b01c94e4e5619c04af4e106a
covik <mate.nakic3@gmail.com>
2021-10-30 12:01:04 +0200

    Add traccar replica count

--- infrastructure/src/production/backend.ts
+++ infrastructure/src/production/backend.ts
@@ -103,6 +103,7 @@ pulumi.interpolate`<?xml version='1.0' encoding='UTF-8'?>
             selector: {
                 matchLabels: labels
             },
+            replicas: 1,
             template: {
                 metadata: {
                     labels:  labels
