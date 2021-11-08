ff3cf60db8c4db975c9dea3912cd0c8737444cce
covik <mate.nakic3@gmail.com>
2021-11-08 17:35:36 +0100

    Simplify type-check

--- infrastructure/src/pulumi/Program.ts
+++ infrastructure/src/pulumi/Program.ts
@@ -2,28 +2,7 @@ import { Stack } from './Stack'
 
 export class Program {
     constructor(stack: Stack) {
-        if(stack === null)
-            throw new TypeError()
-
-        if(typeof stack === 'undefined')
-            throw new TypeError()
-
-        if(Array.isArray(stack))
-            throw new TypeError()
-
-        if(!isNaN(parseInt(stack as any)))
-            throw new TypeError()
-
-        if(typeof stack === 'boolean')
-            throw new TypeError()
-
-        if(typeof stack === 'function')
-            throw new TypeError()
-
-        if(typeof stack === 'string')
-            throw new TypeError()
-
-        if (typeof stack === 'object' && !(stack instanceof Stack))
+        if (stack instanceof Stack === false)
             throw new TypeError()
     }
 }
