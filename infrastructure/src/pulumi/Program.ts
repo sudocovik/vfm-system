4730cee6b6458411866777ecd2221dcb616a6a63
covik <mate.nakic3@gmail.com>
2021-11-08 21:12:01 +0100

    Test pulumi program installs required plugins

--- infrastructure/src/pulumi/Program.ts
+++ infrastructure/src/pulumi/Program.ts
@@ -1,8 +1,25 @@
 import { Stack } from './Stack'
+import { LocalWorkspace } from '@pulumi/pulumi/automation'
+
+const installedDependencies = require('../../package-lock.json').dependencies
+const findDependencyVersion = (wantedDependency: string) => installedDependencies[wantedDependency].version
 
 export class Program {
-    constructor(stack: Stack) {
+    constructor(public readonly stack: Stack) {
         if (stack instanceof Stack === false)
             throw new TypeError()
+    }
+
+    public async execute(): Promise<void> {
+        const vendorStack = await LocalWorkspace.createOrSelectStack({
+            stackName: this.stack.name(),
+            projectName: 'vfm',
+            program: this.stack.resources()
+        })
+
+        await Promise.all([
+            vendorStack.workspace.installPlugin('digitalocean', findDependencyVersion('@pulumi/digitalocean')),
+            vendorStack.workspace.installPlugin('kubernetes', findDependencyVersion('@pulumi/kubernetes'))
+        ])
     }
 }
---
+++ infrastructure/src/pulumi/__tests__/Program.integration.ts
@@ -0,0 +1,47 @@
+import { Program } from '../Program'
+import { Stack } from '../Stack'
+import { LocalWorkspace } from '@pulumi/pulumi/automation'
+
+const stackName = 'integration-testing'
+const projectName = 'vfm'
+const TWO_MINUTES = 2 * 60 * 1000
+
+describe('#Program', () => {
+    beforeAll(() => {
+        process.env.PULUMI_AUTOMATION_API_SKIP_VERSION_CHECK = 'true'
+    })
+
+    afterAll(() => {
+        process.env.PULUMI_AUTOMATION_API_SKIP_VERSION_CHECK = 'false'
+    })
+
+    afterEach(async () => {
+        const stack = await LocalWorkspace.selectStack({
+            stackName,
+            projectName,
+            program: async () => {}
+        })
+
+        const installedPlugins = await stack.workspace.listPlugins()
+        installedPlugins.forEach(({ name }) => stack.workspace.removePlugin(name))
+    })
+
+    it('should install required plugins', async () => {
+        const stack = new Stack(stackName, () => {})
+        const program = new Program(stack)
+
+        await program.execute().then(async () => {
+            const pulumiStack = await LocalWorkspace.selectStack({
+                stackName: stack.name(),
+                projectName: 'vfm',
+                program: stack.resources()
+            })
+
+            const installedPlugins = await pulumiStack.workspace.listPlugins()
+            const pluginNames = installedPlugins.map(plugin => plugin.name)
+
+            expect(pluginNames).toContain('digitalocean')
+            expect(pluginNames).toContain('kubernetes')
+        })
+    }, TWO_MINUTES)
+})
