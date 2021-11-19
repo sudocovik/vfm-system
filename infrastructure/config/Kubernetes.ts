58fc21390ef1a142c4f116fff8eb41389b540b6e
covik <mate.nakic3@gmail.com>
2021-11-19 17:09:09 +0100

    Fix traefik version

--- infrastructure/config/Kubernetes.ts
+++ infrastructure/config/Kubernetes.ts
@@ -3,6 +3,6 @@ import { Project } from './Project'
 export class Kubernetes {
     public static version: string = '1.21.5'
     public static namespace: string = Project.nameLowercase
-    public static traefikVersion: '10.6.0'
+    public static traefikVersion: string = '10.6.2'
     public static containerRegistryCredentials: string = process.env.CLUSTER_CONTAINER_REGISTRY_TOKEN || ''
 }
