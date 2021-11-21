24908dec0cf869c43875c693cf0632442935d3d0
covik <mate.nakic3@gmail.com>
2021-11-21 16:23:43 +0100

    Use history mode instead of hash

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -54,7 +54,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       // transpile: false,
       // publicPath: '/',
@@ -87,7 +87,8 @@ module.exports = configure(function (ctx) {
       open: false, // opens browser window automatically
       client: {
         webSocketURL: 'auto://0.0.0.0:0/ws'
-      }
+      },
+      historyApiFallback: true
     },
 
     // https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
