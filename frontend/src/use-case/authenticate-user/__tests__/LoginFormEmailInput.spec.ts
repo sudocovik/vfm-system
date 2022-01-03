527d80ff75397237b22de47b5ee0bdd663cf3f98
covik <6125218+Covik@users.noreply.github.com>
2022-01-03 16:51:31 +0100

    Extract input assertions and utilities to a file

--- frontend/src/use-case/authenticate-user/__tests__/LoginFormEmailInput.spec.ts
+++ frontend/src/use-case/authenticate-user/__tests__/LoginFormEmailInput.spec.ts
@@ -1,27 +1,13 @@
 import { mount } from '@cypress/vue'
 import LoginFormEmailInput from '../LoginFormEmailInput.vue'
-import { QInput } from 'quasar'
-import { VueWrapper } from '@vue/test-utils'
-
-type InputComponent = VueWrapper<QInput>
-
-const findInputComponent = () => (): InputComponent => Cypress.vueWrapper.findComponent(QInput)
-
-const findProperty = (wantedProperty: string) => (input: InputComponent): string => input.props(wantedProperty) as string
-
-const returnInputValue = () => (input: InputComponent): string => <string>input.vm.modelValue
-
-const changeInputValue = (inputValue: string) => (input: InputComponent) => input.setValue(inputValue)
-
-const findEventsByName = (name: string) => (): string[][] => {
-  const eventSequence = <string[][]>Cypress.vueWrapper.emitted(name)
-  if (eventSequence) return eventSequence
-  else throw new Error(`Event '${name}' never occurred`)
-}
-
-const takeLastEvent = () => (eventSequence: string[][]): string[] => eventSequence[eventSequence.length - 1]
-
-const takeFirstValue = () => (eventData: string[]): string => eventData[0]
+import {
+  componentModelValueShouldBe,
+  textInputChangeValueTo,
+  textInputLabelShouldBe,
+  textInputShouldExist,
+  textInputTypeShouldBe,
+  textInputValueShouldBe
+} from 'src/use-case/authenticate-user/__tests__/input-utilities'
 
 const changeComponentProperties = (changedProperties: Record<string, unknown>) =>
   () => Cypress.vueWrapper.setProps(changedProperties)
@@ -73,48 +59,3 @@ describe('LoginFormEmailInput', () => {
     componentModelValueShouldBe(secondValue)
   })
 })
-
-const textInputShouldExist = (): void => {
-  cy.then(findInputComponent())
-    .then((input: InputComponent) => {
-      expect(input.exists()).to.be.equal(true)
-    })
-}
-
-const textInputTypeShouldBe = (wantedType: string): void => {
-  cy.then(findInputComponent())
-    .then(findProperty('type'))
-    .then((type: string) => {
-      expect(type).to.be.equal(wantedType)
-    })
-}
-
-const textInputLabelShouldBe = (wantedLabel: string): void => {
-  cy.then(findInputComponent())
-    .then(findProperty('label'))
-    .then((label: string) => {
-      expect(label).to.be.equal(wantedLabel)
-    })
-}
-
-const textInputValueShouldBe = (wantedValue: string): void => {
-  cy.then(findInputComponent())
-    .then(returnInputValue())
-    .then((inputValue: string) => {
-      expect(inputValue).to.be.equal(wantedValue)
-    })
-}
-
-const textInputChangeValueTo = (wantedValue: string): void => {
-  cy.then(findInputComponent())
-    .then(changeInputValue(wantedValue))
-}
-
-const componentModelValueShouldBe = (wantedValue: string): void => {
-  cy.then(findEventsByName('update:modelValue'))
-    .then(takeLastEvent())
-    .then(takeFirstValue())
-    .then((firstValue: string) => {
-      expect(firstValue).to.be.equal(wantedValue)
-    })
-}
---
+++ frontend/src/use-case/authenticate-user/__tests__/input-utilities.ts
@@ -0,0 +1,67 @@
+import { VueWrapper } from '@vue/test-utils'
+import { QInput } from 'quasar'
+
+type InputComponent = VueWrapper<QInput>
+
+export const textInputShouldExist = (): void => {
+  cy.then(findInputComponent())
+    .then((input: InputComponent) => {
+      expect(input.exists()).to.be.equal(true)
+    })
+}
+
+export const textInputTypeShouldBe = (wantedType: string): void => {
+  cy.then(findInputComponent())
+    .then(findProperty('type'))
+    .then((type: string) => {
+      expect(type).to.be.equal(wantedType)
+    })
+}
+
+export const textInputLabelShouldBe = (wantedLabel: string): void => {
+  cy.then(findInputComponent())
+    .then(findProperty('label'))
+    .then((label: string) => {
+      expect(label).to.be.equal(wantedLabel)
+    })
+}
+
+export const textInputValueShouldBe = (wantedValue: string): void => {
+  cy.then(findInputComponent())
+    .then(returnInputValue())
+    .then((inputValue: string) => {
+      expect(inputValue).to.be.equal(wantedValue)
+    })
+}
+
+export const textInputChangeValueTo = (wantedValue: string): void => {
+  cy.then(findInputComponent())
+    .then(changeInputValue(wantedValue))
+}
+
+export const componentModelValueShouldBe = (wantedValue: string): void => {
+  cy.then(findEventsByName('update:modelValue'))
+    .then(takeLastEvent())
+    .then(takeFirstValue())
+    .then((firstValue: string) => {
+      expect(firstValue).to.be.equal(wantedValue)
+    })
+}
+
+const findInputComponent = () => (): InputComponent => Cypress.vueWrapper.findComponent(QInput)
+
+const findProperty = (wantedProperty: string) => (input: InputComponent): string => input.props(wantedProperty) as string
+
+const returnInputValue = () => (input: InputComponent): string => <string>input.vm.modelValue
+
+const changeInputValue = (inputValue: string) => (input: InputComponent) => input.setValue(inputValue)
+
+const findEventsByName = (name: string) => (): string[][] => {
+  const eventSequence = <string[][]>Cypress.vueWrapper.emitted(name)
+  if (eventSequence) return eventSequence
+  else throw new Error(`Event '${name}' never occurred`)
+}
+
+const takeLastEvent = () => (eventSequence: string[][]): string[] => eventSequence[eventSequence.length - 1]
+
+const takeFirstValue = () => (eventData: string[]): string => eventData[0]
