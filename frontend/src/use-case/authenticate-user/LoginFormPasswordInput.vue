e8b1f4f0b227398410f5ad3c96fb091b65b312e7
covik <6125218+Covik@users.noreply.github.com>
2022-01-03 17:09:05 +0100

    Add password input field for login form

---
+++ frontend/src/use-case/authenticate-user/LoginFormPasswordInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-input
+    v-model="password"
+    type="password"
+    label="Password"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+
+export default defineComponent({
+  name: 'LoginFormPasswordInput',
+
+  props: {
+    modelValue: {
+      type: String,
+      required: true
+    }
+  },
+
+  emits: ['update:modelValue'],
+
+  setup (props, { emit }) {
+    const password = computed({
+      get: () => props.modelValue,
+      set: (value) => emit('update:modelValue', value)
+    })
+
+    return {
+      password
+    }
+  }
+})
+</script>
---
+++ frontend/src/use-case/authenticate-user/__tests__/LoginFromPasswordInput.spec.ts
@@ -0,0 +1,57 @@
+import { mount } from '@cypress/vue'
+import LoginFormPasswordInput from '../LoginFormPasswordInput.vue'
+import {
+  changeComponentProperties, componentModelValueShouldBe, textInputChangeValueTo,
+  textInputLabelShouldBe,
+  textInputShouldExist,
+  textInputTypeShouldBe,
+  textInputValueShouldBe
+} from 'src/use-case/authenticate-user/__tests__/input-utilities'
+
+describe('LoginFormPasswordInput', () => {
+  it('should render text input', () => {
+    mount(LoginFormPasswordInput)
+
+    textInputShouldExist()
+  })
+
+  it('should mask characters in the text input', () => {
+    mount(LoginFormPasswordInput)
+
+    textInputTypeShouldBe('password')
+  })
+
+  it('should have a placeholder', () => {
+    mount(LoginFormPasswordInput)
+
+    textInputLabelShouldBe('Password')
+  })
+
+  it('should let parent component control the input value', () => {
+    const defaultValue = '123456'
+    const changedValue = '12345678'
+
+    mount(LoginFormPasswordInput, {
+      props: {
+        modelValue: defaultValue
+      }
+    })
+    textInputValueShouldBe(defaultValue)
+
+    cy.then(changeComponentProperties({ modelValue: changedValue }))
+    textInputValueShouldBe(changedValue)
+  })
+
+  it('should notify parent what the user typed', () => {
+    const firstValue = 'user.email@example.com'
+    const secondValue = 'user.email2@example.com'
+
+    mount(LoginFormPasswordInput)
+
+    textInputChangeValueTo(firstValue)
+    componentModelValueShouldBe(firstValue)
+
+    textInputChangeValueTo(secondValue)
+    componentModelValueShouldBe(secondValue)
+  })
+})
