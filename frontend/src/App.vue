e117d3db693a2d16f45cd2d8766259a81d0206c8
covik <6125218+Covik@users.noreply.github.com>
2022-02-05 18:50:56 +0100

    Show main content after successful login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,8 +6,8 @@
       </MainLayout>
     </template>
 
-    <template #unauthenticated>
-      <LoginPage />
+    <template #unauthenticated="{ setAuthenticated }">
+      <LoginPage v-on="{ [onAuthenticationSuccessful]: setAuthenticated }" />
     </template>
   </AuthenticationManager>
 </template>
@@ -17,6 +17,7 @@ import { defineComponent } from 'vue'
 import AuthenticationManager from 'components/AuthenticationManager.vue'
 import MainLayout from 'layouts/MainLayout.vue'
 import LoginPage from './use-case/authenticate-user/LoginPage.vue'
+import { AuthenticationSuccessfulEventName } from './use-case/authenticate-user/AuthenticationSuccessfulEvent'
 
 export default defineComponent({
   name: 'App',
@@ -25,6 +26,8 @@ export default defineComponent({
     AuthenticationManager,
     MainLayout,
     LoginPage
-  }
+  },
+
+  setup: () => ({ onAuthenticationSuccessful: AuthenticationSuccessfulEventName })
 })
 </script>
