df631a9a5a00e02cbfd62014a16a715fe0820db5
covik <6125218+Covik@users.noreply.github.com>
2022-02-15 10:06:55 +0100

    Add modal dialog with date range selection

--- frontend/src/pages/VehicleRouteHistory.vue
+++ frontend/src/pages/VehicleRouteHistory.vue
@@ -1,27 +1,91 @@
 <template>
   <q-page class="q-px-md q-pt-md">
-    <div class="row justify-between">
+    <div
+      v-if="false"
+      class="row justify-between"
+    >
       <div class="col col-12 col-sm-5">
-        <q-date
+        <q-input
           v-model="from"
-          class="full-width"
-          minimal
-        />
+          outlined
+          bg-color="white"
+          @focus="showFromDatePicker = true"
+          @blur="showFromDatePicker = false"
+        >
+          <template #append>
+            <div
+              v-show="showFromDatePicker"
+              style="position: absolute; left: 0; right: 0; top: 56px;"
+            >
+              <q-date
+                v-model="from"
+                minimal
+                class="full-width"
+                bordered
+                flat
+              />
+            </div>
+          </template>
+        </q-input>
       </div>
 
       <div class="col col-12 col-sm-5 offset-sm-2">
-        <q-date
-          v-model="to"
-          class="full-width"
-          minimal
-        />
+        <q-input
+          v-model="from"
+          outlined
+          bg-color="white"
+          @focus="showToDatePicker = true"
+          @blur="showToDatePicker = false"
+        >
+          <template #append>
+            <div
+              v-show="showToDatePicker"
+              style="position: absolute; left: 0; right: 0; top: 56px;"
+            >
+              <q-date
+                v-model="from"
+                minimal
+                class="full-width"
+                bordered
+                flat
+              />
+            </div>
+          </template>
+        </q-input>
       </div>
     </div>
+
+    <div
+      class="full-width bg-red hidden"
+      style="height: 400px;"
+    />
+
+    <div>
+      <q-card
+        class="q-pa-md shadow-1"
+        @click="dialog = true"
+      >
+        Od 13.02.2022. do 14.02.2022.
+      </q-card>
+    </div>
+
+    <q-dialog
+      v-model="dialog"
+      :maximized="isMobile"
+      :transition-show="isMobile ? 'slide-up' : 'flip-up'"
+      :transition-hide="isMobile ? 'slide-down' : 'flip-down'"
+    >
+      <q-date
+        v-model="range"
+        range
+      />
+    </q-dialog>
   </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
+import { useQuasar } from 'quasar'
 
 export default defineComponent({
   name: 'VehicleRouteHistory',
@@ -34,9 +98,20 @@ export default defineComponent({
   },
 
   setup () {
+    const from = ref('2022/02/12')
+    const to = ref('2022/02/13')
+
+    const $q = useQuasar()
+    const isMobile = $q.screen.lt.md
+
     return {
-      from: ref('2022/02/12'),
-      to: ref('2022/02/13')
+      from,
+      to,
+      showFromDatePicker: ref(false),
+      showToDatePicker: ref(false),
+      dialog: ref(false),
+      range: ref({ from, to }),
+      isMobile
     }
   }
 })
