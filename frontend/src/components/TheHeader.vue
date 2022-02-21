dc936083aa30daab5a3fca7c673c2cf2408ea58e
covik <6125218+Covik@users.noreply.github.com>
2022-02-21 23:35:42 +0100

    Style header

--- frontend/src/components/TheHeader.vue
+++ frontend/src/components/TheHeader.vue
@@ -1,9 +1,18 @@
 <template>
-  <q-header>
-    <q-btn
-      icon="mdi-menu"
-      @click="notifyParentMenuToggleWasRequested"
-    />
+  <q-header class="q-px-md q-pt-md bg-transparent">
+    <q-toolbar
+      class="rounded-borders shadow-1 bg-white"
+      style="color: #5E6367"
+    >
+      <q-btn
+        flat
+        dense
+        round
+        icon="mdi-menu"
+        style="color: #3B4042"
+        @click="notifyParentMenuToggleWasRequested"
+      />
+    </q-toolbar>
   </q-header>
 </template>
 
