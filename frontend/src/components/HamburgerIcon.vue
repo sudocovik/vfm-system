4a821440b40e674201612216c31cfaf168ba5d76
covik <6125218+Covik@users.noreply.github.com>
2022-02-22 00:29:49 +0100

    Fix tests failing due to findComponent() not being able to locate hamburger icon

--- frontend/src/components/HamburgerIcon.vue
+++ frontend/src/components/HamburgerIcon.vue
@@ -1,3 +1,11 @@
 <template>
   <q-btn icon="mdi-menu" />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HamburgerIcon'
+})
+</script>
