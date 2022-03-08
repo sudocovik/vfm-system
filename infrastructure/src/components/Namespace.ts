53a68f247409e1c4f21a1c7ef8787630d2000699
covik <6125218+Covik@users.noreply.github.com>
2022-03-08 22:47:14 +0100

    Fix wrong k8s object used to create namespace

--- infrastructure/src/components/Namespace.ts
+++ infrastructure/src/components/Namespace.ts
@@ -3,7 +3,7 @@ import * as k8s from '@pulumi/kubernetes'
 import { Kubernetes } from '../../config'
 
 export function createNamespace (opts?: pulumi.ResourceOptions) {
-  return new k8s.core.v1.Service('primary-namespace', {
+  return new k8s.core.v1.Namespace('primary-namespace', {
     metadata: {
       name: Kubernetes.namespace
     }
