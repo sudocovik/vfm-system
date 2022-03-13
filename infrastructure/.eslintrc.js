177801a3e6cbf080eef8133602501a6c2e807a16
covik <6125218+Covik@users.noreply.github.com>
2022-03-13 10:31:40 +0100

    Allow private constructors (ESLint)

--- infrastructure/.eslintrc.js
+++ infrastructure/.eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'error',
     'no-useless-constructor': 'off', // Does not understand types
     '@typescript-eslint/no-useless-constructor': 'error',
+    'no-empty-function': 'off', // Does not understand types
+    '@typescript-eslint/no-empty-function': ['error', { 'allow': ['private-constructors'] }],
     'no-new': 'off' // Remove once all pulumi objects get created and returned by a function
   },
 
