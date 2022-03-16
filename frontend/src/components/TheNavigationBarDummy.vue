0e8cba43f5d179ba9d3ebbf48003e9e0983bf12d
covik <6125218+Covik@users.noreply.github.com>
2022-03-16 17:14:36 +0100

    Remove default drawer and add dummy navigation bar

---
+++ frontend/src/components/TheNavigationBarDummy.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-tabs
+    no-caps
+    active-color="primary"
+    indicator-color="transparent"
+    class="bg-white text-grey-9"
+  >
+    <q-route-tab
+      to="/"
+      exact
+      name="vehicles"
+      icon="mdi-truck"
+      label="Vozila"
+    />
+    <q-route-tab
+      to="/trailers"
+      exact
+      name="trailers"
+      icon="mdi-truck-trailer"
+      label="Prikolice"
+    />
+    <q-route-tab
+      to="/notifications"
+      exact
+      name="notifications"
+      icon="mdi-bell"
+      label="Obavijesti"
+    />
+    <q-route-tab
+      to="/menu"
+      exact
+      name="menu"
+      icon="mdi-menu"
+      label="Meni"
+    />
+  </q-tabs>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TheNavigationBarDummy'
+})
+</script>
--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,97 +1,38 @@
 <template>
   <q-layout view="hHh Lpr lFf">
-    <TheHeader @[MenuToggle]="toggleLeftDrawer" />
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
+    <TheHeader
+      v-if="false"
+      @[MenuToggle]="toggleLeftDrawer"
+    />
 
     <q-page-container>
       <slot />
     </q-page-container>
+
+    <q-footer>
+      <TheNavigationBarDummy />
+    </q-footer>
   </q-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
 import TheHeader from 'components/TheHeader.vue'
 import { MenuToggleEventName as MenuToggle } from 'components/MenuToggleEvent'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
+import TheNavigationBarDummy from 'components/TheNavigationBarDummy.vue'
 
 export default defineComponent({
   name: 'MainLayout',
 
   components: {
     TheHeader,
-    EssentialLink
+    TheNavigationBarDummy
   },
 
   setup () {
     const leftDrawerOpen = ref(false)
 
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
