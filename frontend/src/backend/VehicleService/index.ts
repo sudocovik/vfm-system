2ffe58ace4c4a0c36f2315cf6c067cec2667b5cb
covik <6125218+Covik@users.noreply.github.com>
2022-03-25 12:45:52 +0100

    Use 'export type' otherwise cypress complains about module with no exports

--- frontend/src/backend/VehicleService/index.ts
+++ frontend/src/backend/VehicleService/index.ts
@@ -1,3 +1,3 @@
 export { VehicleList } from './VehicleList'
 export { VehicleWithoutPosition } from './models/VehicleWithoutPosition'
-export { TraccarDevice } from './models/TraccarDevice'
+export type { TraccarDevice } from './models/TraccarDevice'
