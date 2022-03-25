551ed0913a6094ad4dea02e3396b852e883482f2
covik <6125218+Covik@users.noreply.github.com>
2022-03-25 12:46:33 +0100

    Fix lint error

--- frontend/src/use-case/real-time-vehicle-feed/__tests__/ListOfNotInstalledVehicles.spec.ts
+++ frontend/src/use-case/real-time-vehicle-feed/__tests__/ListOfNotInstalledVehicles.spec.ts
@@ -2,7 +2,6 @@ import { ComponentUnderTest } from 'test/support/api'
 import ListOfNotInstalledVehicles from '../ListOfNotInstalledVehicles.vue'
 import { VehicleWithoutPosition } from 'src/backend/VehicleService'
 import NotInstalledVehicle from '../NotInstalledVehicle.vue'
-import { VueWrapper } from '@vue/test-utils'
 
 describe('ListOfNotInstalledVehicles', () => {
   it('should not render vehicles given list of empty vehicles', () => {
