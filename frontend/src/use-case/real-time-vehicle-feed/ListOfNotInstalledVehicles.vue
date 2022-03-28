e2736e2fa34bb39a21eed822144926c97ddf4bcb
covik <6125218+Covik@users.noreply.github.com>
2022-03-28 14:44:13 +0200

    Finish implementing list of vehicles without GPS tracker installed

--- frontend/src/use-case/real-time-vehicle-feed/ListOfNotInstalledVehicles.vue
+++ frontend/src/use-case/real-time-vehicle-feed/ListOfNotInstalledVehicles.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>
-    <NotInstalledVehicle
-      v-if="vehicles.length"
-      :name="vehicles[0].licensePlate()"
-    />
-  </div>
+  <NotInstalledVehicle
+    v-for="vehicle in notInstalledVehicles"
+    :key="vehicle.id()"
+    :name="vehicle.licensePlate()"
+    :class="$attrs['class']"
+  />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent, PropType } from 'vue'
 import NotInstalledVehicle from './NotInstalledVehicle.vue'
+import { VehicleWithoutPosition } from 'src/backend/VehicleService'
 
 export default defineComponent({
   name: 'ListOfNotInstalledVehicles',
@@ -18,10 +19,18 @@ export default defineComponent({
     NotInstalledVehicle
   },
 
+  inheritAttrs: false,
+
   props: {
     vehicles: {
-      type: Array,
+      type: Array as PropType<VehicleWithoutPosition[]>,
       required: true
+    }
+  },
+
+  setup (props) {
+    return {
+      notInstalledVehicles: computed(() => props.vehicles.filter(vehicle => vehicle instanceof VehicleWithoutPosition))
     }
   }
 })
--- frontend/src/use-case/real-time-vehicle-feed/__tests__/ListOfNotInstalledVehicles.spec.ts
+++ frontend/src/use-case/real-time-vehicle-feed/__tests__/ListOfNotInstalledVehicles.spec.ts
@@ -2,6 +2,12 @@ import { ComponentUnderTest } from 'test/support/api'
 import ListOfNotInstalledVehicles from '../ListOfNotInstalledVehicles.vue'
 import { VehicleWithoutPosition } from 'src/backend/VehicleService'
 import NotInstalledVehicle from '../NotInstalledVehicle.vue'
+import { VueWrapper } from '@vue/test-utils'
+
+const appropriateVehicles = [
+  new VehicleWithoutPosition(1, 'ZD000AA', '000000', false),
+  new VehicleWithoutPosition(2, 'ZD111BB', '111111', false)
+]
 
 describe('ListOfNotInstalledVehicles', () => {
   it('should not render vehicles given list of empty vehicles', () => {
@@ -14,20 +20,52 @@ describe('ListOfNotInstalledVehicles', () => {
   })
 
   it('should render only vehicles without GPS tracker installed', () => {
-    const appropriateVehicle = new VehicleWithoutPosition(1, 'ZD000AA', '000000', false)
     const vehicles = [
-      appropriateVehicle,
-      { licensePlate: 'ZD1111BB' }
+      ...appropriateVehicles,
+      { licensePlate: 'ZD-FAKE-00' }
     ]
 
     ComponentUnderTest.is(ListOfNotInstalledVehicles).withProperties({ vehicles }).mount()
 
     cy.then(() => {
       const renderedVehicles = Cypress.vueWrapper.findAllComponents(NotInstalledVehicle)
-      expect(renderedVehicles).to.have.length(1)
+      expect(renderedVehicles.length).to.equal(appropriateVehicles.length)
 
-      const firstVehicle = renderedVehicles[0]
-      expect(firstVehicle.props('name')).to.equal(appropriateVehicle.licensePlate())
+      appropriateVehicles.forEach((vehicle, i) => {
+        const componentKey = getComponentKey(renderedVehicles[i])
+        const nameProperty = <string>renderedVehicles[i].props('name')
+
+        expect(componentKey).to.equal(vehicle.id())
+        expect(nameProperty).to.equal(vehicle.licensePlate())
+      })
+    })
+  })
+
+  describe('should apply class to all children NotInstalledVehicle components', () => {
+    (['my-first-class', 'my-second-class']).forEach((targetClass, i) => {
+      it(`case ${i + 1}: class named '${targetClass}'`, () => {
+        ComponentUnderTest.is(ListOfNotInstalledVehicles)
+          .withProperties({ vehicles: appropriateVehicles })
+          .withAttributes({ class: targetClass })
+          .mount()
+
+        cy.then(() => {
+          const notInstalledVehicleComponents = Cypress.vueWrapper.findAllComponents(NotInstalledVehicle)
+          notInstalledVehicleComponents.forEach(component => expect(component.classes()).includes(targetClass))
+        })
+      })
     })
   })
 })
+
+function getComponentKey (component: VueWrapper) {
+  type VmOptions = {
+    $: {
+      vnode: {
+        key: number
+      }
+    }
+  }
+
+  return (<VmOptions>component.vm).$.vnode.key
+}
