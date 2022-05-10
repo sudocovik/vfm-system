13050a52888d25e369b4b22002b0724b8d132396
covik <6125218+Covik@users.noreply.github.com>
2022-05-10 14:16:11 +0200

    Add direction arrow with different colors & rotation in vehicle map icon

--- frontend/src/use-case/real-time-vehicle-feed/VehicleAsIcon.ts
+++ frontend/src/use-case/real-time-vehicle-feed/VehicleAsIcon.ts
@@ -1,6 +1,27 @@
-export function createVehicleIcon (): string {
+export const colors = {
+  yellow: {
+    stroke: '#d78200',
+    fill: '#ff9a00'
+  },
+  green: {
+    stroke: '#00a706',
+    fill: '#00cc07'
+  }
+}
+
+export function createVehicleIcon (moving = false, ignition = false, rotation = 0): string {
   return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-    <circle cx="16" cy="16" data-cy="background-stroke" r="16" fill="#aaaaaa" />
-    <circle cx="16" cy="16" data-cy="background-fill" r="15" fill="#ebebeb" />
+    <g data-cy="rotation-group" transform="rotate(${rotation} 16 16)">
+        <circle cx="16" cy="16" data-cy="background-stroke" r="16" fill="#aaaaaa" />
+        <circle cx="16" cy="16" data-cy="background-fill" r="15" fill="#ebebeb" />
+
+        ${moving ? `<path data-cy="direction-arrow"
+stroke="${colors[ignition ? 'green' : 'yellow'].stroke}"
+stroke-width="1"
+fill="${colors[ignition ? 'green' : 'yellow'].fill}"
+d="M20 20.725a.94.94 0 0 1-.55-.17l-6.9-4.56a1 1 0 0 0-1.1 0l-6.9 4.56a1 1 0 0 1-1.44-1.28l8-16a1 1 0 0 1 1.78 0l8 16a1 1 0 0 1-.23 1.2A1 1 0 0 1 20 20.725z"
+transform="translate(4, 3)"
+/>` : ''}
+    </g>
 </svg>`
 }
--- frontend/src/use-case/real-time-vehicle-feed/__tests__/VehicleAsIcon.spec.ts
+++ frontend/src/use-case/real-time-vehicle-feed/__tests__/VehicleAsIcon.spec.ts
@@ -1,4 +1,4 @@
-import { createVehicleIcon } from '../VehicleAsIcon'
+import { colors, createVehicleIcon } from '../VehicleAsIcon'
 
 describe('VehicleAsIcon', () => {
   beforeEach(resetScene)
@@ -85,6 +85,121 @@ describe('VehicleAsIcon', () => {
         cy.get('@root').dataCy('background-fill').should('have.attr', 'fill').should('not.equal', firstCircleFillColor)
       })
     })
+
+    specify('circles should be under rotation group', () => {
+      const icon = createVehicleIcon(true, false)
+      mountIcon(icon)
+
+      cy.dataCy('rotation-group').dataCy('background-stroke').should('be.visible')
+      cy.dataCy('rotation-group').dataCy('background-fill').should('be.visible')
+    })
+  })
+
+  describe('Direction arrow', () => {
+    it('should be rendered when property \'moving\' is true', () => {
+      const icon = createVehicleIcon(true)
+      mountIcon(icon)
+
+      cy.dataCy('direction-arrow').should('exist')
+    })
+
+    it('should not be rendered when property \'moving\' is false', () => {
+      const icon = createVehicleIcon(false)
+      mountIcon(icon)
+
+      cy.dataCy('direction-arrow').should('not.exist')
+    })
+
+    it('should be node of type \'path\'', () => {
+      const icon = createVehicleIcon(true)
+      mountIcon(icon)
+
+      cy.dataCy('direction-arrow').then($el => {
+        expect($el.get(0).nodeName).equal('path')
+      })
+    })
+
+    it('should have a stroke color', () => {
+      const icon = createVehicleIcon(true)
+      mountIcon(icon)
+
+      cy.dataCy('direction-arrow').should('have.attr', 'stroke') // not interested in actual color, covered by other tests
+    })
+
+    it('should have a stroke width of 1', () => {
+      const icon = createVehicleIcon(true)
+      mountIcon(icon)
+
+      cy.dataCy('direction-arrow').should('have.attr', 'stroke-width', 1)
+    })
+
+    it('should have a fill color', () => {
+      const icon = createVehicleIcon(true)
+      mountIcon(icon)
+
+      cy.dataCy('direction-arrow').should('have.attr', 'fill') // not interested in actual color, covered by other tests
+    })
+
+    it('should have a shape', () => {
+      const icon = createVehicleIcon(true)
+      mountIcon(icon)
+
+      cy.dataCy('direction-arrow').should('have.attr', 'd') // shape is a bit complex, test it visually
+      cy.dataCy('direction-arrow').should('have.attr', 'transform') // required to be properly centered but depends on 'd' attribute so value is not tested
+    })
+
+    it('should be under rotation group', () => {
+      const icon = createVehicleIcon(true, false)
+      mountIcon(icon)
+
+      cy.dataCy('rotation-group').dataCy('direction-arrow').should('be.visible')
+    })
+
+    describe('Rotation', () => {
+      it('should rotate around center of the icon', () => {
+        const icon = createVehicleIcon(true, false)
+        mountIcon(icon)
+
+        const center = '16 16'
+
+        cy.dataCy('rotation-group').should('have.attr', 'transform').should('match', new RegExp(`rotate\\(\\d ${center}\\)`))
+      })
+
+      it('should have a default value of 0', () => {
+        const icon = createVehicleIcon(true, false)
+        mountIcon(icon)
+
+        iconRotationShouldBe(0)
+      })
+
+      const rotations = [15, 270]
+      rotations.forEach((rotation, i) => {
+        it(`case ${i + 1}: rotation = ${rotation}`, () => {
+          const icon = createVehicleIcon(true, false, rotation)
+          mountIcon(icon)
+
+          iconRotationShouldBe(rotation)
+        })
+      })
+    })
+
+    describe('Colors', () => {
+      it('should have a yellow-like stroke & fill color when property \'ignition\' is false', () => {
+        const icon = createVehicleIcon(true, false)
+        mountIcon(icon)
+
+        cy.dataCy('direction-arrow').should('have.attr', 'stroke', colors.yellow.stroke)
+        cy.dataCy('direction-arrow').should('have.attr', 'fill', colors.yellow.fill)
+      })
+
+      it('should have a green-like stroke & fill color when property \'ignition\' is true', () => {
+        const icon = createVehicleIcon(true, true)
+        mountIcon(icon)
+
+        cy.dataCy('direction-arrow').should('have.attr', 'stroke', colors.green.stroke)
+        cy.dataCy('direction-arrow').should('have.attr', 'fill', colors.green.fill)
+      })
+    })
   })
 })
 
@@ -94,4 +209,8 @@ function resetScene () {
 
 function mountIcon (icon: string) {
   cy.document({ log: false }).invoke({ log: false }, 'write', icon)
+}
+
+function iconRotationShouldBe (rotation: number) {
+  cy.get('svg').dataCy('rotation-group').should('have.attr', 'transform').should('contain', `rotate(${rotation}`)
 }
