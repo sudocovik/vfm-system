74e939ef95de461b4d811367d33035817cf07de5
covik <6125218+Covik@users.noreply.github.com>
2022-05-11 09:29:26 +0200

    Extract creation of direction arrow and stop indicator to their own methods

--- frontend/src/use-case/real-time-vehicle-feed/VehicleAsIcon.ts
+++ frontend/src/use-case/real-time-vehicle-feed/VehicleAsIcon.ts
@@ -9,27 +9,36 @@ export const colors = {
   }
 }
 
-export function createVehicleIcon (moving = false, ignition = false, rotation = 0): string {
+const createDirectionArrow = (fillColor: string, strokeColor: string) =>
+`<path data-cy="direction-arrow"
+  fill="${fillColor}"
+  stroke="${strokeColor}"
+  stroke-width="1"
+  d="M20 20.725a.94.94 0 0 1-.55-.17l-6.9-4.56a1 1 0 0 0-1.1 0l-6.9 4.56a1 1 0 0 1-1.44-1.28l8-16a1 1 0 0 1 1.78 0l8 16a1 1 0 0 1-.23 1.2A1 1 0 0 1 20 20.725z"
+  transform="translate(4, 3)"
+/>`
+
+const createStopIndicator = (fillColor: string, strokeColor: string) =>
+`<rect data-cy="stop-indicator"
+  fill="${fillColor}"
+  stroke="${strokeColor}"
+  stroke-width="1"
+  width="12"
+  height="12"
+  x="10"
+  y="10"
+/>`
+
+export function createVehicleIcon (moving = false, ignition = false, rotationInDegrees = 0): string {
+  const rotation = moving ? rotationInDegrees : 0
+  const { stroke, fill } = colors[ignition ? 'green' : 'yellow']
+
   return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-    <g data-cy="rotation-group" transform="rotate(${moving ? rotation : 0} 16 16)">
+    <g data-cy="rotation-group" transform="rotate(${rotation} 16 16)">
         <circle cx="16" cy="16" data-cy="background-stroke" r="16" fill="#aaaaaa" />
         <circle cx="16" cy="16" data-cy="background-fill" r="15" fill="#ebebeb" />
 
-        ${moving ? `<path data-cy="direction-arrow"
-stroke="${colors[ignition ? 'green' : 'yellow'].stroke}"
-stroke-width="1"
-fill="${colors[ignition ? 'green' : 'yellow'].fill}"
-d="M20 20.725a.94.94 0 0 1-.55-.17l-6.9-4.56a1 1 0 0 0-1.1 0l-6.9 4.56a1 1 0 0 1-1.44-1.28l8-16a1 1 0 0 1 1.78 0l8 16a1 1 0 0 1-.23 1.2A1 1 0 0 1 20 20.725z"
-transform="translate(4, 3)"
-/>` : `<rect data-cy="stop-indicator"
-stroke="${colors[ignition ? 'green' : 'yellow'].stroke}"
-stroke-width="1"
-fill="${colors[ignition ? 'green' : 'yellow'].fill}"
-width="12"
-height="12"
-x="10"
-y="10"
-/>`}
+        ${moving ? createDirectionArrow(fill, stroke) : createStopIndicator(fill, stroke)}
     </g>
 </svg>`
 }
