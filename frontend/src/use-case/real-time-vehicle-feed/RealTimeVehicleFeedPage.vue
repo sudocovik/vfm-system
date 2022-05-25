19e2c4015a1246f0c54e1485f01d03467f366db4
covik <6125218+Covik@users.noreply.github.com>
2022-05-25 23:23:09 +0200

    Add a couple of dummy GeoLocatedVehicles

--- frontend/src/use-case/real-time-vehicle-feed/RealTimeVehicleFeedPage.vue
+++ frontend/src/use-case/real-time-vehicle-feed/RealTimeVehicleFeedPage.vue
@@ -4,6 +4,41 @@
       {{ title }}
     </div>
 
+    <GeoLocatedVehicle
+      license-plate="ZD547GC"
+      address="Ulica Ante Starčevića 2, 23000 Zadar, HR"
+      :latitude="44.107205"
+      :longitude="15.241929"
+      :ignition="true"
+      :moving="false"
+      :course="50"
+      :speed="Speed.fromKnots(0)"
+    />
+
+    <GeoLocatedVehicle
+      license-plate="ZD815LK"
+      address="E55, 30174 Venezia, IT"
+      :latitude="45.519794"
+      :longitude="12.252026"
+      :ignition="true"
+      :moving="true"
+      :course="220"
+      :speed="Speed.fromKnots(30)"
+      class="q-mt-md"
+    />
+
+    <GeoLocatedVehicle
+      license-plate="ZD409AJ"
+      address="93354 Siegenburg, DE"
+      :latitude="48.739043"
+      :longitude="11.856284"
+      :ignition="false"
+      :moving="false"
+      :course="220"
+      :speed="Speed.fromKnots(0)"
+      class="q-mt-md"
+    />
+
     <ListOfNotInstalledVehicles
       :vehicles="vehiclesWithoutPosition"
       class="q-mt-md"
@@ -17,10 +52,12 @@ import { useMeta } from 'quasar'
 import { t } from 'boot/i18n'
 import ListOfNotInstalledVehicles from './ListOfNotInstalledVehicles.vue'
 import { VehicleWithoutPosition } from '../../backend/VehicleService'
+import GeoLocatedVehicle from './GeoLocatedVehicle.vue'
+import { Speed } from 'src/support/measurement-units/speed'
 
 export default defineComponent({
   name: 'RealTimeVehicleFeedPage',
-  components: { ListOfNotInstalledVehicles },
+  components: { GeoLocatedVehicle, ListOfNotInstalledVehicles },
   setup () {
     const title = t('vehicles')
 
@@ -34,7 +71,8 @@ export default defineComponent({
 
     return {
       title,
-      vehiclesWithoutPosition
+      vehiclesWithoutPosition,
+      Speed
     }
   }
 })
