8cbecef4dd4448b4f6bc28cfb56d8f2393b99820
covik <6125218+Covik@users.noreply.github.com>
2022-05-25 22:41:28 +0200

    Support environment variables from file in frontend local environment

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -46,6 +46,8 @@ module.exports = configure(function (ctx) {
     build: {
       vueRouterMode: 'history', // available values: 'hash', 'history'
 
+      env: require('dotenv').config().parsed,
+
       // transpile: false,
       // publicPath: '/',
 
