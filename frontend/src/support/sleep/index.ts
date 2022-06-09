c571931dd0bc7fd0e5696ecb3a6aed64bcd220b9
covik <6125218+Covik@users.noreply.github.com>
2022-06-09 16:44:38 +0200

    Simplify sleep() function header

--- frontend/src/support/sleep/index.ts
+++ frontend/src/support/sleep/index.ts
@@ -1,5 +1,3 @@
 export const sleep = {
-  now: (ms: number): Promise<void> => {
-    return new Promise(resolve => setTimeout(resolve, ms))
-  }
+  now: (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
 }
