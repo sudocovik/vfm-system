18551c73e39008ccecfdf2230f2288229c6f65c5
covik <6125218+Covik@users.noreply.github.com>
2022-06-09 10:11:32 +0200

    Add comment explaining await has not been tested

--- frontend/src/support/short-poll/index.ts
+++ frontend/src/support/short-poll/index.ts
@@ -9,5 +9,6 @@ export const shortPoll = {
     await resultHandler(result)
     await shortPoll.sleep(delayInMilliseconds)
     await shortPoll.do(action, resultHandler, delayInMilliseconds)
+    // |-- this await has not been covered by unit tests but eslint fails if it's missing
   }
 }
