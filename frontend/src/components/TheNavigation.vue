0f6bfc9d04dfdc32577f5af4c01375ee66e76e5b
covik <6125218+Covik@users.noreply.github.com>
2022-07-04 12:13:17 +0200

    Revert "Temporarily disable scrollarea to see what happens in safari"
    This reverts commit af0dc74e3f037a7bc8879db2deda86fcedc68b2a.

--- frontend/src/components/TheNavigation.vue
+++ frontend/src/components/TheNavigation.vue
@@ -21,84 +21,89 @@
 
     <q-separator />
 
-    <q-list>
-      <q-item
-        to="/"
-        data-cy="item-vehicles"
-      >
-        <div class="hidden">
-          {{ $t('vehicles') }}
-        </div>
+    <q-scroll-area
+      style="flex: 1"
+      data-cy="scrollable-area"
+    >
+      <q-list>
+        <q-item
+          to="/"
+          data-cy="item-vehicles"
+        >
+          <div class="hidden">
+            {{ $t('vehicles') }}
+          </div>
 
-        <q-avatar icon="mdi-truck" />
-      </q-item>
+          <q-avatar icon="mdi-truck" />
+        </q-item>
 
-      <q-item
-        to="/trailers"
-        data-cy="item-trailers"
-      >
-        <div class="hidden">
-          {{ $t('trailers') }}
-        </div>
+        <q-item
+          to="/trailers"
+          data-cy="item-trailers"
+        >
+          <div class="hidden">
+            {{ $t('trailers') }}
+          </div>
 
-        <q-avatar icon="mdi-truck-trailer" />
-      </q-item>
+          <q-avatar icon="mdi-truck-trailer" />
+        </q-item>
 
-      <q-item
-        to="/drivers"
-        data-cy="item-drivers"
-      >
-        <div class="hidden">
-          {{ $t('drivers') }}
-        </div>
+        <q-item
+          to="/drivers"
+          data-cy="item-drivers"
+        >
+          <div class="hidden">
+            {{ $t('drivers') }}
+          </div>
 
-        <q-avatar icon="mdi-account-tie-hat" />
-      </q-item>
+          <q-avatar icon="mdi-account-tie-hat" />
+        </q-item>
 
-      <q-item
-        to="/services"
-        data-cy="item-services"
-      >
-        <div class="hidden">
-          {{ $t('services') }}
-        </div>
+        <q-item
+          to="/services"
+          data-cy="item-services"
+        >
+          <div class="hidden">
+            {{ $t('services') }}
+          </div>
 
-        <q-avatar icon="mdi-hammer-wrench" />
-      </q-item>
+          <q-avatar icon="mdi-hammer-wrench" />
+        </q-item>
 
-      <q-item
-        to="/notifications"
-        data-cy="item-notifications"
-      >
-        <div class="hidden">
-          {{ $t('notifications') }}
-        </div>
+        <q-item
+          to="/notifications"
+          data-cy="item-notifications"
+        >
+          <div class="hidden">
+            {{ $t('notifications') }}
+          </div>
 
-        <q-avatar icon="mdi-bell" />
-      </q-item>
+          <q-avatar icon="mdi-bell" />
+        </q-item>
 
-      <q-item
-        to="/settings"
-        data-cy="item-settings"
-      >
-        <div class="hidden">
-          {{ $t('settings') }}
-        </div>
+        <q-item
+          to="/settings"
+          data-cy="item-settings"
+        >
+          <div class="hidden">
+            {{ $t('settings') }}
+          </div>
 
-        <q-avatar icon="mdi-cog" />
-      </q-item>
+          <q-avatar icon="mdi-cog" />
+        </q-item>
 
-      <q-item
-        to="/logout"
-        data-cy="item-logout"
-      >
-        <div class="hidden">
-          {{ $t('logout') }}
-        </div>
+        <q-item
+          to="/logout"
+          data-cy="item-logout"
+        >
+          <div class="hidden">
+            {{ $t('logout') }}
+          </div>
 
-        <q-avatar icon="mdi-power" />
-      </q-item>
-    </q-list>
+          <q-avatar icon="mdi-power" />
+        </q-item>
+      </q-list>
+    </q-scroll-area>
   </q-drawer>
 </template>
 
