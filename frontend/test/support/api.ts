967e6495aec76bfdd35eb2feb4bbf1a6ef4c2323
covik <6125218+Covik@users.noreply.github.com>
2022-07-05 16:58:31 +0200

    Add type for $slots Vue property

--- frontend/test/support/api.ts
+++ frontend/test/support/api.ts
@@ -1,4 +1,5 @@
 import { ComponentConstructor } from 'quasar'
+import { VNode } from 'vue'
 
 export * from './ComponentUnderTest'
 export * from './InputField'
@@ -9,3 +10,9 @@ export * from './Random'
 export * from './getComponentKey'
 
 export type QuasarComponentPublicApi<T> = T extends ComponentConstructor<infer E> ? E : T
+
+export type AvailableSlots = {
+  $slots: {
+    default: () => VNode[]
+  }
+}
